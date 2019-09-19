5d35e180d88bf7a4d84c05eb99e6edde367cf1ca
Eray Erdin <eraygezer.94@gmail.com>
2019-09-19 14:23:03 +0300

    added "tgcli bot send message" and "tgcli bot send document" tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,11 +1,14 @@
 import io
 import json
+import os
 import typing
 
-import tgcli.request.bot
-
 import pytest
+import requests
 import requests_mock
+
+import tgcli.request.bot
+from tgcli.cli import cli as cli_root
 
 
 @pytest.fixture
@@ -14,6 +17,11 @@ def mock_adapter() -> requests_mock.Adapter:
     Returns a mock adapter.
     """
     return requests_mock.Adapter()
+
+
+@pytest.fixture
+def cli():
+    return cli_root
 
 
 @pytest.fixture
@@ -310,5 +318,35 @@ def file_factory(request):
         file = open(file_path, "rb")
         request.addfinalizer(lambda: file.close())
         return file
+
+    return factory
+
+
+@pytest.fixture
+def bot_token() -> typing.Union[str, None]:
+    """
+    Returns TELEGRAM_BOT_TOKEN environment variable.
+    """
+    return os.environ.get("TELEGRAM_BOT_TOKEN")
+
+
+@pytest.fixture
+def receiver_id() -> typing.Union[str, None]:
+    """
+    Returns TELEGRAM_RECEIVER environment variable.
+    """
+    return os.environ.get("TELEGRAM_RECEIVER")
+
+
+@pytest.fixture
+def invoke_message_factory():
+    """
+    A factory to generate message for Telegram.
+    """
+
+    def factory(klass: typing.ClassVar, method: typing.Callable):
+        return "A message invoked by `{}::{}`.".format(
+            klass.__name__, method.__name__
+        )
 
     return factory
---
+++ tests/test_cli/test_bot_send.py
@@ -0,0 +1,114 @@
+from click.testing import CliRunner
+
+from tests.test_cli import (
+    SKIPIF_BOT_RECEIVER_NOT_EXISTS,
+    SKIPIF_BOT_TOKEN_NOT_EXISTS,
+    SKIPIF_HAS_NOT_CONNECTED,
+)
+from tgcli.cli.bot.send import MESSAGE_FORMATS
+
+
+class TestMessage:
+    @SKIPIF_HAS_NOT_CONNECTED
+    @SKIPIF_BOT_TOKEN_NOT_EXISTS
+    @SKIPIF_BOT_RECEIVER_NOT_EXISTS
+    def test_html(
+        self,
+        cli_runner: CliRunner,
+        cli,
+        receiver_id: str,
+        invoke_message_factory,
+    ):
+        args = (
+            "bot",
+            "send",
+            "-r",
+            receiver_id,
+            "--format",
+            "html",
+            "message",
+            invoke_message_factory(self.__class__, self.test_html),
+        )
+        result = cli_runner.invoke(cli, args)
+        assert result.exit_code == 0
+
+    @SKIPIF_HAS_NOT_CONNECTED
+    @SKIPIF_BOT_TOKEN_NOT_EXISTS
+    @SKIPIF_BOT_RECEIVER_NOT_EXISTS
+    def test_markdown(
+        self,
+        cli_runner: CliRunner,
+        cli,
+        receiver_id: str,
+        invoke_message_factory,
+    ):
+        args = (
+            "bot",
+            "send",
+            "-r",
+            receiver_id,
+            "--format",
+            "markdown",
+            "message",
+            invoke_message_factory(self.__class__, self.test_markdown),
+        )
+        result = cli_runner.invoke(cli, args)
+        assert result.exit_code == 0
+
+
+class TestDocument:
+    @SKIPIF_HAS_NOT_CONNECTED
+    @SKIPIF_BOT_TOKEN_NOT_EXISTS
+    @SKIPIF_BOT_RECEIVER_NOT_EXISTS
+    def test_vanilla(self, cli_runner: CliRunner, cli, receiver_id: str):
+        args = (
+            "bot",
+            "send",
+            "-r",
+            receiver_id,
+            "document",
+            "tests/resources/file.png",
+        )
+        result = cli_runner.invoke(cli, args)
+        assert result.exit_code == 0
+
+    @SKIPIF_HAS_NOT_CONNECTED
+    @SKIPIF_BOT_TOKEN_NOT_EXISTS
+    @SKIPIF_BOT_RECEIVER_NOT_EXISTS
+    def test_thumbnail(self, cli_runner: CliRunner, cli, receiver_id: str):
+        args = (
+            "bot",
+            "send",
+            "-r",
+            receiver_id,
+            "document",
+            "setup.py",
+            "--thumbnail",
+            "tests/resources/file.png",
+        )
+        result = cli_runner.invoke(cli, args)
+        assert result.exit_code == 0
+        # todo does not send thumbnail
+
+    @SKIPIF_HAS_NOT_CONNECTED
+    @SKIPIF_BOT_TOKEN_NOT_EXISTS
+    @SKIPIF_BOT_RECEIVER_NOT_EXISTS
+    def test_message(
+        self,
+        cli_runner: CliRunner,
+        cli,
+        receiver_id: str,
+        invoke_message_factory,
+    ):
+        args = (
+            "bot",
+            "send",
+            "-r",
+            receiver_id,
+            "document",
+            "setup.py",
+            "-m",
+            invoke_message_factory(self.__class__, self.test_message),
+        )
+        result = cli_runner.invoke(cli, args)
+        assert result.exit_code == 0
