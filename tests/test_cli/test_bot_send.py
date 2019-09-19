699770960ce2c2e6b35bd261cd4d326240120b18
Eray Erdin <eraygezer.94@gmail.com>
2019-09-19 14:31:03 +0300

    added "bot send photo" tests

--- tests/test_cli/test_bot_send.py
+++ tests/test_cli/test_bot_send.py
@@ -112,3 +112,43 @@ class TestDocument:
         )
         result = cli_runner.invoke(cli, args)
         assert result.exit_code == 0
+
+
+class TestPhoto:
+    @SKIPIF_HAS_NOT_CONNECTED
+    @SKIPIF_BOT_TOKEN_NOT_EXISTS
+    @SKIPIF_BOT_RECEIVER_NOT_EXISTS
+    def test_vanilla(self, cli_runner: CliRunner, cli, receiver_id: str):
+        args = (
+            "bot",
+            "send",
+            "-r",
+            receiver_id,
+            "photo",
+            "tests/resources/file.png",
+        )
+        result = cli_runner.invoke(cli, args)
+        assert result.exit_code == 0
+
+    @SKIPIF_HAS_NOT_CONNECTED
+    @SKIPIF_BOT_TOKEN_NOT_EXISTS
+    @SKIPIF_BOT_RECEIVER_NOT_EXISTS
+    def test_message(
+        self,
+        cli_runner: CliRunner,
+        cli,
+        receiver_id: str,
+        invoke_message_factory,
+    ):
+        args = (
+            "bot",
+            "send",
+            "-r",
+            receiver_id,
+            "photo",
+            "tests/resources/file.png",
+            "-m",
+            invoke_message_factory(self.__class__, self.test_vanilla),
+        )
+        result = cli_runner.invoke(cli, args)
+        assert result.exit_code == 0
