02d7a86a81fac8a04ca294656a84e94ff934d45e
Eray Erdin <eraygezer.94@gmail.com>
2021-06-02 10:25:14 +0300

    added logger

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
 use tgcli::cli::{get_app, match_app};
 
 fn main() {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            if cfg!(debug_assertions) {
+                out.finish(format_args!(
+                    "[{}][{}] {}",
+                    record.level(),
+                    record.target(),
+                    message
+                ))
+            } else {
+                match record.level() {
+                    log::Level::Error => out.finish(format_args!("Error: {}", message)),
+                    log::Level::Warn => out.finish(format_args!("Warning: {}", message)),
+                    _ => out.finish(format_args!("{}", message)),
+                }
+            }
+        })
+        .level(if cfg!(debug_assertions) {
+            log::LevelFilter::Debug
+        } else {
+            log::LevelFilter::Info
+        })
+        .chain(std::io::stdout())
+        .apply()
+        .unwrap();
+
     let app = get_app();
     match_app(app);
 }
