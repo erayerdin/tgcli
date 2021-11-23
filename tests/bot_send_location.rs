9fc93f65460a14f1f6265f0ef626f2c98f1933c2
Eray Erdin <eraygezer.94@gmail.com>
2021-11-23 11:34:47 +0300

    tested send location

---
+++ tests/bot_send_location.rs
@@ -0,0 +1,42 @@
+use std::env;
+
+use assert_cmd::Command;
+
+// Copyright 2021 Eray Erdin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#[macro_use]
+extern crate rstest;
+
+#[fixture]
+fn binary() -> Command {
+    let _ = dotenv::dotenv();
+    Command::cargo_bin("tgcli").expect("Could not find tgcli binary.")
+}
+
+#[rstest]
+fn send_location(mut binary: Command) {
+    let assertion = binary.args([
+        "bot",
+        "send",
+        "location",
+        "-x", "38.42",
+        "-y", "27.14",
+        "--receiver",
+        &env::var("TELEGRAM_RECEIVER")
+            .expect("TELEGRAM_RECEIVER environment variable could not be found. Please create .env file and define it.")
+    ]).assert();
+
+    assertion.success();
+}
