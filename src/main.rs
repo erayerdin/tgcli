2af0cfd62c5663d6f167de557caedbd87e2a89d1
Eray Erdin <eraygezer.94@gmail.com>
2021-11-24 23:54:44 +0300

    separated stdout and stderr chains

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,40 @@ fn main() {
         // stdout chain
         .chain(
             fern::Dispatch::new()
+                .filter(|metadata| {
+                    metadata.level() != log::LevelFilter::Error
+                        || metadata.level() != log::LevelFilter::Warn
+                })
+                .format(|out, message, record| {
+                    if cfg!(debug_assertions) {
+                        out.finish(format_args!(
+                            "[{}][{}] {}",
+                            record.level(),
+                            record.target(),
+                            message
+                        ))
+                    } else {
+                        match record.level() {
+                            log::Level::Error => out.finish(format_args!("Error: {}", message)),
+                            log::Level::Warn => out.finish(format_args!("Warning: {}", message)),
+                            _ => out.finish(format_args!("{}", message)),
+                        }
+                    }
+                })
+                .level(if cfg!(debug_assertions) {
+                    log::LevelFilter::Trace
+                } else {
+                    log::LevelFilter::Info
+                })
+                .chain(std::io::stdout()),
+        )
+        // stderr chain
+        .chain(
+            fern::Dispatch::new()
+                .filter(|metadata| {
+                    metadata.level() == log::LevelFilter::Error
+                        || metadata.level() == log::LevelFilter::Warn
+                })
                 .format(|out, message, record| {
                     if cfg!(debug_assertions) {
                         out.finish(format_args!(
