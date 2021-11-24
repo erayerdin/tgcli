e9fa4f92225600138b35dee6536e2bc486963871
Eray Erdin <eraygezer.94@gmail.com>
2021-11-24 23:48:25 +0300

    unified stdout chain

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,32 @@ use tgcli::cli::{get_app, match_app};
 
 fn main() {
     fern::Dispatch::new()
-        .format(|out, message, record| {
-            if cfg!(debug_assertions) {
-                out.finish(format_args!(
-                    "[{}][{}] {}",
-                    record.level(),
-                    record.target(),
-                    message
-                ))
-            } else {
-                match record.level() {
-                    log::Level::Error => out.finish(format_args!("Error: {}", message)),
-                    log::Level::Warn => out.finish(format_args!("Warning: {}", message)),
-                    _ => out.finish(format_args!("{}", message)),
-                }
-            }
-        })
-        .level(if cfg!(debug_assertions) {
-            log::LevelFilter::Trace
-        } else {
-            log::LevelFilter::Info
-        })
-        .chain(std::io::stdout())
+        // stdout chain
+        .chain(
+            fern::Dispatch::new()
+                .format(|out, message, record| {
+                    if cfg!(debug_assertions) {
+                        out.finish(format_args!(
+                            "[{}][{}] {}",
+                            record.level(),
+                            record.target(),
+                            message
+                        ))
+                    } else {
+                        match record.level() {
+                            log::Level::Error => out.finish(format_args!("Error: {}", message)),
+                            log::Level::Warn => out.finish(format_args!("Warning: {}", message)),
+                            _ => out.finish(format_args!("{}", message)),
+                        }
+                    }
+                })
+                .level(if cfg!(debug_assertions) {
+                    log::LevelFilter::Trace
+                } else {
+                    log::LevelFilter::Info
+                })
+                .chain(std::io::stdout()),
+        )
         .apply()
         .unwrap();
 
