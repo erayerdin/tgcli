1b34275187604df8ceb75cfc93c7252d7953d48d
Eray Erdin <eraygezer.94@gmail.com>
2023-03-02 02:36:46 +0300

    change set_logger arg to Verbocity

--- src/cli/logging.rs
+++ src/cli/logging.rs
@@ -13,8 +13,17 @@ pub(crate) fn set_logger(
     // 2 - Debug, Info, Warn, Error + Self Target + Level Labels + Location Labels
     // 3 - Trace, Debug, Info, Warn, Error + Self Target + Level Labels + Location Labels
     // 4 - Trace, Debug, Info, Warn, Error + All Targets + Level Labels + Location Labels
-    verbosity: u8,
+    verbosity_level: clap_verbosity_flag::Verbosity,
 ) -> Result<(), log::SetLoggerError> {
+    let verbosity: u8 = match verbosity_level.log_level_filter() {
+        log::LevelFilter::Off => 0,
+        log::LevelFilter::Trace => 1,
+        log::LevelFilter::Debug => 2,
+        log::LevelFilter::Info => 3,
+        log::LevelFilter::Warn => 4,
+        log::LevelFilter::Error => 5,
+    };
+
     let colors = ColoredLevelConfig::new()
         .error(Color::BrightRed)
         .warn(Color::Yellow)
@@ -36,52 +45,29 @@ pub(crate) fn set_logger(
                     metadata.level() != log::LevelFilter::Error
                         || metadata.level() != log::LevelFilter::Warn
                 })
-                .format(move |out, message, record| {
-                    // if cfg!(debug_assertions) {
-                    //     out.finish(format_args!(
-                    //         "[{}][{}] {}",
-                    //         record.level(),
-                    //         record.target(),
-                    //         message
-                    //     ))
-                    // } else {
-                    //     match record.level() {
-                    //         log::Level::Error => out.finish(format_args!("Error: {}", message)),
-                    //         log::Level::Warn => out.finish(format_args!("Warning: {}", message)),
-                    //         _ => out.finish(format_args!("{}", message)),
-                    //     }
-                    // }
-                    match verbosity {
-                        1 => out.finish(format_args!(
-                            "[{levelname}] {message}",
-                            levelname = record.level(),
-                            message = message,
-                        )),
-                        2..=u8::MAX => out.finish(format_args!(
-                            "[{levelname}][{targetname}] {message}",
-                            levelname = record.level(),
-                            targetname = record.target(),
-                            message = message,
-                        )),
-                        0 => out.finish(format_args!(
-                            "\x1B[{}m{}\x1B[0m",
-                            colors.get_color(&record.level()).to_fg_str(),
-                            message
-                        )),
-                    }
+                .format(move |out, message, record| match verbosity {
+                    1 => out.finish(format_args!(
+                        "[{levelname}] {message}",
+                        levelname = record.level(),
+                        message = message,
+                    )),
+                    2..=u8::MAX => out.finish(format_args!(
+                        "[{levelname}][{targetname}] {message}",
+                        levelname = record.level(),
+                        targetname = record.target(),
+                        message = message,
+                    )),
+                    0 => out.finish(format_args!(
+                        "\x1B[{}m{}\x1B[0m",
+                        colors.get_color(&record.level()).to_fg_str(),
+                        message
+                    )),
                 })
-                .level(
-                    // if cfg!(debug_assertions) {
-                    //     log::LevelFilter::Trace
-                    // } else {
-                    //     log::LevelFilter::Info
-                    // }
-                    match verbosity {
-                        0 => log::LevelFilter::Info,
-                        1 | 2 => log::LevelFilter::Debug,
-                        _ => log::LevelFilter::Trace,
-                    },
-                )
+                .level(match verbosity {
+                    0 => log::LevelFilter::Info,
+                    1 | 2 => log::LevelFilter::Debug,
+                    _ => log::LevelFilter::Trace,
+                })
                 .chain(std::io::stdout()),
         )
         // stderr chain
